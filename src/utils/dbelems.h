58ea234f37a58c40c2227e211615eb55feaf49bc
Dorian <dorianemmanuelturner@gmail.com>
2025-07-13 23:08:41 +0100

    Feat: Function headers defined for NoSQL style storage and README updated to reflect new design

--- src/utils/dbelems.h
+++ src/utils/dbelems.h
@@ -20,23 +20,25 @@ struct VersionNode {
 };
 
 struct Field {
+    pthread_rwlock_t lock;
     char *name;
     VersionNode versions;
 };
 
 struct Document {
-    HashMap fields; // char* → VersionNode (Field)
-    HashMap subcollections; // char* → VersionNode(Collection) (optional)
+    pthread_rwlock_t lock;
+    HashMap fields;          // char* → VersionNode(Field)
+    HashMap subcollections;  // char* → VersionNode(Collection)
 };
 
 struct Collection {
     pthread_rwlock_t lock;
-    HashMap documents; // char* → VersionNode(Document)
+    HashMap documents;       // char* → VersionNode(Document)
 };
 
 struct Table {
     pthread_rwlock_t lock;
-    HashMap collections; // char* → VersionNode(Collection)
+    HashMap collections;     // char* → VersionNode(Collection)
 };
 
 #define RECORD_TYPE_SET     1
@@ -54,24 +56,38 @@ struct Record {
 extern uint64_t GLOBAL_VERSION;
 extern pthread_mutex_t GLOBAL_VERSION_LOCK;
 
+// Memory allocation and freeing helpers
 VersionNode version_node_create(void *value, uint64_t global_version, uint64_t local_version);
 void version_node_free_chain(VersionNode head);
 
-int field_set(Field field, void *value, uint64_t global_version);
-void *field_get(Field field, uint64_t global_version);
+Field field_create(const char *name);
 void field_free(Field field);
 
 Document document_create(void);
 void document_free(Document doc);
-Field document_get_field(Document doc, const char *key, int create);
 
 Collection collection_create(void);
 void collection_free(Collection coll);
-Document collection_get_document(Collection coll, const char *doc_id, int create);
 
 Table table_create(void);
 void table_free(Table table);
-Collection table_get_collection(Table table, const char *name, int create);
+
+// Getters and setters
+
+int field_set(Field field, void *value, uint64_t global_version);
+void *field_get(Field field, uint64_t local_version);
+
+Field document_get_field(Document doc, const char *key, uint64_t local_version);
+int document_set_field(Document doc, const char *key, void *value, uint64_t global_version);
+Collection document_get_subcollection(Document doc, const char *name, uint64_t local_version);
+int document_set_subcollection(Document doc, const char *name, Collection subcoll, uint64_t global_version);
+
+Document collection_get_document(Collection coll, const char *name, uint64_t local_version);
+int collection_set_document(Collection coll, const char *name, Document doc, uint64_t global_version);
+
+Collection table_get_collection(Table table, const char *name, uint64_t local_version);
+int table_set_collection(Table table, const char *name, Collection coll, uint64_t global_version);
+
 
 #endif
 
